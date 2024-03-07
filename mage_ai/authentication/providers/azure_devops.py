1f9e9a4cd95d99c35b59a8c9589ba5dee701364d
dy46 <davidyang1996@gmail.com>
2024-03-07 15:12:33 -0800

    [dy] Azure devops

--- mage_ai/authentication/providers/azure_devops.py
+++ mage_ai/authentication/providers/azure_devops.py
@@ -1,14 +1,17 @@
-from mage_ai.authentication.oauth.constants import AZURE_DEVOPS_INSTANCE, ProviderName
+from mage_ai.authentication.oauth.constants import (
+    AZURE_DEVOPS_ORGANIZATION,
+    ProviderName,
+)
 from mage_ai.authentication.providers.active_directory import ADProvider
 
 
 class AzureDevopsProvider(ADProvider):
     provider = ProviderName.AZURE_DEVOPS
-    scope = 'vso.tokenadministration vso.code_write vso.profile'  # noqa: E501
+    scope = '499b84ac-1321-427f-aa17-267ca6975798/.default'  # noqa: E501
 
     def __init__(self):
         super().__init__()
-        if not AZURE_DEVOPS_INSTANCE:
+        if not AZURE_DEVOPS_ORGANIZATION:
             raise Exception(
-                'Azure DevOps instance is empty. '
-                'Make sure the AZURE_DEVOPS_INSTANCE environment variable is set.')
+                'Azure DevOps organization is empty. '
+                'Make sure the AZURE_DEVOPS_ORGANIZATION environment variable is set.')
