7efe68f0f0e374343aa472e473a1ac8c6a85b497
dy46 <davidyang1996@gmail.com>
2024-03-07 15:20:00 -0800

    [dy] Clean project

--- mage_ai/authentication/providers/azure_devops.py
+++ mage_ai/authentication/providers/azure_devops.py
@@ -7,7 +7,9 @@ from mage_ai.authentication.providers.active_directory import ADProvider
 
 class AzureDevopsProvider(ADProvider):
     provider = ProviderName.AZURE_DEVOPS
-    scope = '499b84ac-1321-427f-aa17-267ca6975798/.default'  # noqa: E501
+    # This is a hardcoded scope value that is used to get Git access to the Azure DevOps project.
+    # This should not be changed unless this scope stops working properly.
+    scope = '499b84ac-1321-427f-aa17-267ca6975798/.default'
 
     def __init__(self):
         super().__init__()
